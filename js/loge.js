1411644ef88cf37809720621d9695065b7c5d3f5
kazimehedihasan <kazimehedihasan458@.com>
2023-08-15 10:13:39 +0600

    first commit

---
+++ js/loge.js
@@ -0,0 +1,15 @@
+document.getElementById("btn-submit").addEventListener("click", function () {
+  const emailField = document.getElementById("user-email");
+  const email = emailField.value;
+
+  // password
+  const passwordField = document.getElementById("user-password");
+  const password = passwordField.value;
+  // console.log(email, password);
+
+  if (email === "kazimehedihasan33@gmail.com" && password === "secret") {
+    window.location.href = "Bondur.Bank";
+  } else {
+    alert("tui je amar bondu amar vabtei sorom kore, tui password vule gecho");
+  }
+});
