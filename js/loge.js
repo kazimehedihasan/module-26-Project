0c1dc8f90c5e73429e1a335649dfe74e1454dcdd
kazimehedihasan <kazimehedihasan458@.com>
2023-08-15 12:57:51 +0600

    first commit

--- js/loge.js
+++ js/loge.js
@@ -13,3 +13,28 @@ document.getElementById("btn-submit").addEventListener("click", function () {
     alert("tui je amar bondu amar vabtei sorom kore, tui password vule gecho");
   }
 });
+
+
+
+
+
+
+
+
+
+// ----------------
+document.getElementById('btn-Deposit').addEventListener('click', function(){
+  // console.log('mehedi');
+  const DepositField = document.getElementById('Deposit-field');
+  const DepositAmount = DepositField.value;
+  console.log(DepositAmount);
+  // --
+  const DepositTotalElement = document.getElementById('Deposit-total');
+  const DepositTotal = DepositTotalElement.innerText;
+DepositTotalElement.innerText = DepositAmount;
+})
+
+
+// --------
+
+
